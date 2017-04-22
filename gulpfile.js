70df456ed2a705f998688c481458fd52c7edd636
Chingiz Mizambekov <chinjka_m@hotmail.com>
2017-04-22 20:16:47 +0200

    Gulp config added for browser sync on save of any in the APP folder

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+
+gulp.task('watch', ['browser-sync'], function () {
+});
+
+gulp.task('browser-sync', ['nodemon'], function() {
+	browserSync.init(null, {
+		proxy: "http://localhost:5000",
+        files: ["app/**/*.*"],
+        port: 7000
+	});
+});
+
+gulp.task('nodemon', function (cb) {
+	
+	var started = false;
+	
+	return nodemon({
+		script: 'server.js'
+	}).on('start', function () {
+		// to avoid nodemon being started multiple times
+		// thanks @matthisk
+		if (!started) {
+			cb();
+			started = true; 
+		} 
+	});
+});
